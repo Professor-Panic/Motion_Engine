b6dd17c82de2096e0072accf1622e2d922d6f8d8
Kaizen-Mage <simulationworld4@gmail.com>
2026-05-21 22:30:33 +0300

    Added nholmann json and published the first draft of the animationEngine.Now supporting circles

---
+++ build/main.d
@@ -0,0 +1,5 @@
+build/main.o: src/main.cpp external/raylib/include/raylib.h \
+ external/raylib/include/raymath.h include/json/json.hpp
+external/raylib/include/raylib.h:
+external/raylib/include/raymath.h:
+include/json/json.hpp:
